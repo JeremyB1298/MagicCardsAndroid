4142fba059f75d3125e777c3ec441daf89a17f0d
Bailly <jeremy.bailly@hotmail.com>
2018-10-17 11:57:21 +0200

    add log out button facebook

--- app/src/main/java/com/example/lpiem/magiccards/MenuActivity.java
+++ app/src/main/java/com/example/lpiem/magiccards/MenuActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.squareup.picasso.Picasso;
 
@@ -19,6 +22,7 @@ public class MenuActivity extends AppCompatActivity {
     private TextView tvUserEmail;
     private ImageView ivUserPicture;
     private JSONObject response, profile_pic_data, profile_pic_url;
+    private Button bLogOut;
 
 
 
@@ -31,6 +35,7 @@ public class MenuActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String jsondata = intent.getStringExtra("userProfile");
         Log.w("Jsondata", jsondata);
+        bLogOut=(Button) findViewById(R.id.bLogOut);
         tvUserName = (TextView) findViewById(R.id.tvUserName);
         ivUserPicture = (ImageView) findViewById(R.id.ivUserPicture);
         tvUserEmail = (TextView) findViewById(R.id.tvUserEmail);
@@ -49,5 +54,11 @@ public class MenuActivity extends AppCompatActivity {
 
     }
 
+    public void logOut(View view){
 
+        LoginManager.getInstance().logOut();
+        Intent intent = new Intent(MenuActivity.this,MainActivity.class);
+        startActivity(intent);
+
+    }
 }
