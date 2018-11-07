82db6175d25952c4e745d240184b00e199475929
Bailly <jeremy.bailly@hotmail.com>
2018-11-07 17:12:26 +0100

    user

--- app/src/main/java/controllers/InterfaceMagicCardAPI.java
+++ app/src/main/java/controllers/InterfaceMagicCardAPI.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.List;
 
 import Models.Example;
+import Models.User;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -13,5 +14,9 @@ public interface InterfaceMagicCardAPI {
             @Path("id") int id
     );
 
+    @GET("/MagicCard/web/index.php/googleConnexion/{googleId}")
+    Call<User> getUserByGoogle(
+            @Path("googleId") int googleId
+    );
 
 }
