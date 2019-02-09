54f3832fa5c3723731d9a49e1e16f689c44d3585
jeremy <jeremy.bailly@etu.univ-lyon1.fr>
2019-02-09 21:13:12 +0100

    [Add] fragment shop design

--- app/src/main/java/Fragments/Fragment3.kt
+++ app/src/main/java/Fragments/Fragment3.kt
@@ -1,7 +1,6 @@
 package Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +13,7 @@ class Fragment3: androidx.fragment.app.Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var rootView = inflater!!.inflate(R.layout.fragment_3, container, false)
+        var rootView = inflater!!.inflate(R.layout.fragment_shop, container, false)
         return rootView
     }
 }
